4079716c22c40a7e4267b423f650e567f6a2f833
Rodrigo Ramírez <roid85@gmail.com>
2022-05-05 09:52:22 -0500

    Add -h parameter

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -4,6 +4,8 @@ CMD_MYSQL=/usr/bin/mysql
 
 MYSQL_USER=<SET USERNAME>
 MYSQL_PASSWORD=<SET PASSWORD>
+MYSQL_HOST=127.0.0.1
+
 
 LOG_FILE=/var/log/backup-cron.log
 
@@ -11,10 +13,10 @@ DST_BACKUP=/web/backup
 
 for DB in $($CMD_MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -e 'show databases' -s --skip-column-names);
 do
+    CURRENT_DATE=$(/usr/bin/date "+%F-%H:%M:%S")
+    echo "$CURRENT_DATE START BACKUP DB: $DB" >> $LOG_FILE
+    $CMD_MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST $DB> $DST_BACKUP/$DB.$CURRENT_DATE.sql
     CURRENT_DATE=$(/usr/bin/date "+%F-%H-%M-%S")
-    echo "----- $CURRENT_DATE START BACKUP DB: $DB -----" >> $LOG_FILE
-    $CMD_MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD $DB> $DST_BACKUP/$DB.$CURRENT_DATE.sql >> $LOG_FILE
-    CURRENT_DATE=$(/usr/bin/date "+%F-%H-%M-%S")
-    echo "----- $CURRENT_DATE END   BACKUP DB: $DB -----" >> $LOG_FILE
+    echo "$CURRENT_DATE END   BACKUP DB: $DB" >> $LOG_FILE
 done
  
